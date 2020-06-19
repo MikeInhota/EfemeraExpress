c753a927e1a484d4b1aa3b5f935ffcb0892d56a8
Mike Inhota <mike.batista@etecia.com.br>
2020-06-19 14:48:15 -0300

    Criando Contato.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,16 +3,20 @@ const homeController = {
         let servicos = [
             {nome: 'Desenvolvimento Web', imagem: '/imagens/undraw_dev_focus.svg'},
             {nome: 'Marketing Digital', imagem: '/imagens/undraw_social_dashboard.svg'},
-            {nome: 'Consultoria UX', imagem: '/imagens/undraw_mobile_apps.svg'}
+            {nome: 'Consultoria UX', imagem: '/imagens/undraw_mobile_apps.svg'},
         ];
         let banners = [
             '/imagens/banner_ux.jpg',
             '/imagens/full_stack_banner.jpg',
-            '/imagens/banner.jpg'
+            '/imagens/banner.jpg',
         ];
         res.render('index', {
             title: 'Home', listarServicos: servicos
         });
+    }
+    contato: (req, res) => {
+        let {nome, email, mensagem} = req.body;
+        res.render('contato', {nome, email, mensagem});
     }
 };
 
